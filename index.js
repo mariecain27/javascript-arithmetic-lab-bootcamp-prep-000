bd5c4915ffc0c38377b862dad662340ee8cd5fad
mariecain27@gmail.com <mariecain27@gmail.com>
2018-05-03 23:48:37 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,28 @@
+function add(a, b) {
+  return a + b
+  add (1, 80)
+}
+
+function subtract(a, b) {
+  return a - b
+  subtract (60, 40)
+}
+
+function multiply(a, b) {
+  return a * b
+  multiply (2, 3.4)
+}
+
+function divide(a, b) {
+  return a / b
+  divide (5.0, 2.5)
+}
+
+function inc() {
+  var n = 1
+    parseInt(n, 10)
+  n++ 
+  return n
+}
+
+
