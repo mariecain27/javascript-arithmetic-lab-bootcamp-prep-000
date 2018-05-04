eaeac225f060539d91bb5abd558a0ce4f123ddf1
mariecain27@gmail.com <mariecain27@gmail.com>
2018-05-04 01:02:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,11 @@ function dec(a) {
 }
 
 function makeInt(a) {
-  if (parseInt(a, 10)) {
+  if (isNaN(a)) {
+    return true 
+  } else {
+  if (
+    parseInt(a, 10)) {
     return 0
   } else {
   parseInt(a, 10) 
